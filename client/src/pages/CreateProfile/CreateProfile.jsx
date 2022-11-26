29a131dd20a9179c63f05ab5e488fba157bc6c7c
Sauvik Nath <sauviknath10@gmail.com>
2022-11-26 14:23:44 +0530

    stepper initial commit

--- client/src/pages/CreateProfile/CreateProfile.jsx
+++ client/src/pages/CreateProfile/CreateProfile.jsx
@@ -1,9 +1,26 @@
+import { Box, Grid, Paper } from '@mui/material'
 import React from 'react'
+import { Component } from 'react'
 
-function CreateProfile() {
-  return (
-    <div>CreateProfile</div>
-  )
+export default class CreateProfile extends Component {
+  render(){
+    return (
+      <Grid container style={{
+        display: "flex",
+        flexFlow: "row Wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "98vh",
+      }}>
+        <Grid item xs={12} sm={7}>
+          <form>
+            <Box p={2} mb={2} component={Paper}>
+               Stepper works here
+            </Box>
+          </form>
+        </Grid>
+      </Grid>
+    )
+  }
 }
 
-export default CreateProfile
